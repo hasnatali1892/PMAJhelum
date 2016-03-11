e6d8d4261db35ace082bf6844c23284028652d9e
hasnatali1892 <alihasnar51@gmail.com>
2016-03-12 02:54:26 +0500

    Upload

---
+++ PMAPunjabERP/PMAControler/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PMAControler
+{
+    public class Class1
+    {
+        public void fun()
+        {
+            string x = "abc";
+        }
+    }
+}
---
+++ PMAPunjabERP/PMAPunjabERP/WebForm1.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PMAControler;
+
+namespace PMAPunjabERP.PMAPunjabERP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Class1 y = new Class1();
+            y.fun();
+        }
+    }
+}
