d97c47a359d2ed189f71d6e790df3e804e819a49
hasnatali1892 <alihasnar51@gmail.com>
2016-03-12 14:27:47 +0500

    asdf

---
+++ PMAPunjabERP/PMAPunjabERP/PMAMasterPage.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PMAPunjabERP
+{
+    public partial class PMAMasterPage : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
